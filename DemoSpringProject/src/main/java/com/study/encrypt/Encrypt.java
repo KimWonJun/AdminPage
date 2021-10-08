5efb82a33d2b6db969a705018f563525a6429fd9
KimWonJun <kwj0313im@naver.com>
2021-10-08 22:58:10 +0900

    회원등록 시 비번에 SHA256 암호화 적용 (진행중)

---
+++ DemoSpringProject/src/main/java/com/study/encrypt/Encrypt.java
@@ -0,0 +1,27 @@
+package com.study.encrypt;
+
+import java.security.MessageDigest;
+
+public class Encrypt 
+{
+	public String encryptSHA256(String pwd)
+	{
+		try 
+		{
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(pwd.getBytes());
+			byte[] bytes = md.digest();
+
+			StringBuilder builder = new StringBuilder();
+		    for (byte b: bytes) {
+		      builder.append(String.format("%02x", b));
+		    }
+		    System.out.println(builder.toString());
+		    return builder.toString();
+
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
